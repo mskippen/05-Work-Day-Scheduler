57fb781d558870d94a7b39eb6fb5298917995d89
Megan Skippen <79444992+mskippen@users.noreply.github.com>
2021-04-05 20:10:37 +1000

    HTMLDateAtTop added

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    //when page loads, do something
+    var m = moment()
+    var DateAtTop = m.format("dddd, MMMM Do");
+    $("#currentDay").html(DateAtTop)
+
+    
+})
+
+
+
+
